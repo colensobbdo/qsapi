04d9284d706a1b049c44efb6ab42fc8453b96465
Jay Baker <logikal@gmail.com>
2016-09-26 17:08:07 +1300

    feat(API): update main API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
-export default {
-    init: () => {
-        return this
+import Schema from './schema'
+import Fetch from './fetch'
+
+var Qsapi = (options, schema, initialData) => {
+
+    var qsapi = {}
+    qsapi.options = options
+    qsapi.schema = schema
+    qsapi.initialData = initialData
+
+
+    if ( ! initialData) {
+        qsapi.fetch = () => {
+            return new Promise((res, rej) => {
+                Fetch.req(options)
+                    .then((response) => {
+                        return response
+                    })
+                    .then((response) => {
+                        res(Schema.parse(response.data, schema))
+                    })
+                    .catch((err) => {
+                        rej(err)
+                    })
+            })
+        }
     }
+    else {
+        qsapi.fetch = () => {
+            return new Promise((res, rej) => {
+                try {
+                    var result = Schema.parse(initialData, schema)
+                    res(result)
+                }
+                catch (e) {
+                    rej(e)
+                }
+            })
+        }
+    }
+
+    return qsapi
 }
+
+Qsapi.Schema = Schema
+Qsapi.Fetch = Fetch
+
+module.exports = Qsapi
